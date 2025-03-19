d8b55ee8b78bc455b44a954eec170cc8cb4fde9e
Kelvin Lim <lim.kelvino@gmail.com>
2025-03-18 21:07:07 -0500

    change font

--- main.js
+++ main.js
@@ -98,9 +98,9 @@ var timeline = [];
 var instructions1 = {
   type: jsPsychHtmlButtonResponse,
   stimulus: `
-    <p style="font-size:50px;"><b>GoStop Task</b></p>
-    <p style="font-size:25px;">The goal of this task is to correctly respond when the number presented on the screen is the same as the number that was presented right before it.</p>
-    <p style="font-size:25px;">Press next for more instructions.</p>
+    <p style="font-size:30px;"><b>GoStop Task</b></p>
+    <p style="font-size:20px;">The goal of this task is to correctly respond when the number presented on the screen is the same as the number that was presented right before it.</p>
+    <p style="font-size:20px;">Press next for more instructions.</p>
   `,
   choices: ['NEXT'],
   button_html: '<button class="button" style="font-size: 24px; padding: 15px 30px;">%choice%</button>',
