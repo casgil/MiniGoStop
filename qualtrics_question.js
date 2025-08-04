f9e7edcfa80bee8ced47c7add9480d01d6cb2096
Kelvin O. Lim <kolim@umn.edu>
2025-08-04 14:36:30 -0500

    add docs

--- qualtrics_question.js
+++ qualtrics_question.js
@@ -1,24 +1,3 @@
-# MiniGoStop
-
-This repository contains the code for a GoStop task written in jspsych. 
-It was forked from the version
-written by Florence Larkin.  
-
-The reason for the fork was to change the size of the buttons and fonts to make it
-easier to run on mobile.  A future enhancement will be to have it adapt to platforms
-dynamically.
-
-The main deployment for this task has been using Qualtrics.
-
-## Configuration with Qualtrics
-
-Steps:
-
-1. Create a new Block to hold the task.
-2. Create a question.
-3. Add the following Javascript
-   
-```
 Qualtrics.SurveyEngine.addOnload(function()
 {
 /*Place your JavaScript here to run when the page loads*/
@@ -102,14 +81,3 @@ Qualtrics.SurveyEngine.addOnUnload(function()
 	/*Place your JavaScript here to run when the page is unloaded*/
 
 });
-
-```
-
-4. From the Survey tab, select SurveyFlow and add "Set Embedded Data"
-5. Add a New Field GoStop and set it's value to -1.  This is how the data from the 
-   jspsych task is passed to qualtrics to be saved in the response.  The EmbeddedData GoStop
-   corresponds to the entry in the question JavaScript where the data is returned from jspsych.
-
-   The code can be found at line 53 in `qualtrics_question.js`.
-
-   [View the code component here](https://github.com/kelvinlim/MiniGoStop/blob/qualtrics_question.js#L53-L58)
